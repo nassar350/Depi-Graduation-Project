c6634691c45e37d7c1253df9b0a9b603dac4332a
Shimaa Adel <eng.shimaa2005@gmail.com>
2025-10-16 18:41:38 +0300

    add entities with their congiurations

---
+++ Eventify.Core/Entities/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Core.Entities
+{
+    public class Payment
+    {
+        public int BookingId { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Booking Booking { get; set; }
+    }
+}
---
+++ Eventify.Repository/Data/Configurations/PaymentConfigurations.cs
@@ -0,0 +1,34 @@
+﻿using Eventify.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Repository.Data.Configurations
+{
+    public class PaymentConfigurations : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.ToTable("Payments");
+
+            builder.HasKey(p => p.BookingId);
+
+            builder.Property(p => p.TotalPrice).HasColumnType("decimal(5,2").IsRequired();
+            builder.Property(p => p.PaymentMethod).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Status).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.DateTime).IsRequired();
+
+            builder.HasOne(p => p.Booking)
+                .WithOne(b => b.Payment).
+                HasForeignKey<Payment>(p => p.BookingId)
+                .IsRequired();
+
+
+        }
+    }
+
+}
