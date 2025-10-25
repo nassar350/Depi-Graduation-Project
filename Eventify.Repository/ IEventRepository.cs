daea5a7719777ee2538d06f80359a00c0a0ff3ba
Habiba Khaled <hk188789@gmail.com>
2025-10-25 07:10:02 +0300

    added event feature (controller + service + repository)

---
+++ Eventify.Repository/ IEventRepository.cs
@@ -0,0 +1,9 @@
+﻿public interface IEventRepository
+{
+    Task<IEnumerable<Event>> GetAllAsync();
+    Task<Event> GetByIdAsync(int id);
+    Task AddAsync(Event entity);
+    void Update(Event entity);
+    void Delete(Event entity);
+    Task<bool> ExistsAsync(int id);
+}
---
+++ Eventify.Service/IEventService.cs
@@ -0,0 +1,8 @@
+﻿public interface IEventService
+{
+    Task<IEnumerable<EventDto>> GetAllAsync();
+    Task<EventDto> GetByIdAsync(int id);
+    Task<EventDto> CreateAsync(CreateEventDto dto);
+    Task<bool> UpdateAsync(int id, UpdateEventDto dto);
+    Task<bool> DeleteAsync(int id);
+}
