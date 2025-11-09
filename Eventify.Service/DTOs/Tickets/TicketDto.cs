87de930eed0ae7b6dae29b7dd3010a6362e9c246
Mohamed Nassar <126024444+nassar350@users.noreply.github.com>
2025-11-09 23:02:11 +0200

    Moving DTOs To Service Layer

---
+++ Eventify.Service/DTOs/Tickets/TicketDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Eventify.APIs.DTOs.Tickets
+{
+    public class TicketDto
+    {
+        public int ID { get; set; }
+
+        public string Place { get; set; }
+
+        public string Type { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int EventId { get; set; }
+
+        public int? BookingId { get; set; }
+    }
+
+}
---
+++ Eventify.Service/Mappings/CategoryProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Eventify.APIs.DTOs.Categories;
+using Eventify.Core.Entities;
+
+namespace Eventify.APIs.Mappings
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+
+            CreateMap<CreateCategoryDto, Category>()
+                .ForMember(dest => dest.Booked, opt => opt.MapFrom(_ => 0));
+
+            CreateMap<UpdateCategoryDto, Category>()
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<Category, CategoryDetailsDto>();
+        }
+    }
+
+}
