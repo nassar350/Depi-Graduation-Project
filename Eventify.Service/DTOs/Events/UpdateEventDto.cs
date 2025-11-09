1e2ec982f5166cd70de75048372c67304f2f828b
Mohamed Nassar <126024444+nassar350@users.noreply.github.com>
2025-11-09 23:02:11 +0200

    Moving DTOs To Service Layer

---
+++ Eventify.Service/DTOs/Events/UpdateEventDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Eventify.APIs.DTOs.Events
+{
+    public class UpdateEventDto
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Address { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public List<int>? CategoryIds { get; set; }
+    }
+}
---
+++ Eventify.Service/DTOs/Users/UserRegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using Eventify.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eventify.APIs.DTOs.Users
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+
+}
