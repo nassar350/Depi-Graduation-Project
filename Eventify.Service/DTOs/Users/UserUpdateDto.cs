16b159f1d55d58fb1b82da6fe9b2326d406dd946
Ahmed-Elzamly <aelzamly888@gmail.com>
2025-11-14 15:13:11 +0200

    End with user CRUD

--- Eventify.Service/DTOs/Users/UserUpdateDto.cs
+++ Eventify.Service/DTOs/Users/UserUpdateDto.cs
@@ -5,14 +5,12 @@ namespace Eventify.Service.DTOs.Users
 {
     public class UserUpdateDto
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
-        public string? Phone { get; set; }
-
-        public Role? Role { get; set; }
+        public string PhoneNumber { get; set; }
 
     }
 
---
+++ Eventify.Service/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using Eventify.Core.Entities;
+using Eventify.Service.DTOs.Users;
+using Eventify.Service.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ServiceResult<IEnumerable<UserDto>>> GetAllAsync();
+        Task<ServiceResult<UserDto>> GetByIdAsync(int id);
+        Task<ServiceResult<bool>> DeleteAsync(int id);
+        Task<ServiceResult<UserUpdateDto>> UpdateAsync(int id, UserUpdateDto user);
+
+
+    }
+}
